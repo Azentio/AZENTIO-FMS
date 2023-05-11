bb0196221c741f8af6befc1b01fee75aaea1b10b
ININDC00482 <ININDC00482@PSK9T3U-DESKTOP.azentio.com>
2023-05-11 12:56:34 +0530

    user credentials added

--- AzentioAutomationFramework_FMS/src/test/java/Runner/TestApplication.java
+++ AzentioAutomationFramework_FMS/src/test/java/Runner/TestApplication.java
@@ -15,7 +15,7 @@ import utilities.ExtentTestManager;
                            "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
                            "rerun:ReRunScenarios/FailedReRun.txt"}
                //dryRun = false //to check compilation errors                                      
-               , tags="@LoginSADS"
+               , tags="@LOGIN"
 
 )
 public class TestApplication extends AbstractTestNGCucumberTests {
